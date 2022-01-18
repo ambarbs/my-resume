ba24fa8fb3bc149eb1160e8c2a49f15cc39bd687
Ambar Bose <ambar.bs@gmail.com>
2022-01-17 20:32:25 -0500

    Updates

---
+++ precache-manifest.dc0bc367e56bf955614f76acd64818ab.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea274ec9c4966148280ccc9e81661386",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "fe010fa0a465fb60b6d2",
+    "url": "/my-resume/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "368c69275c70f3093801",
+    "url": "/my-resume/static/js/2.a7eba15e.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/my-resume/static/js/2.a7eba15e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9312be6061a8747537",
+    "url": "/my-resume/static/js/3.df422d58.chunk.js"
+  },
+  {
+    "revision": "fe010fa0a465fb60b6d2",
+    "url": "/my-resume/static/js/main.2f2a5764.chunk.js"
+  },
+  {
+    "revision": "7d23260773fc3a67751a",
+    "url": "/my-resume/static/js/runtime-main.9dbc6ba1.js"
+  },
+  {
+    "revision": "c114aacc31c46938dbb414011a54b7ee",
+    "url": "/my-resume/static/media/Indience.c114aacc.png"
+  },
+  {
+    "revision": "f52dbcdfaa6feb4743eca52dbb6fed15",
+    "url": "/my-resume/static/media/coreLogic.f52dbcdf.jpg"
+  },
+  {
+    "revision": "ef88eabe293836a7b4361713df7cfaeb",
+    "url": "/my-resume/static/media/iag.ef88eabe.png"
+  },
+  {
+    "revision": "0e92c8f08ffddf198b61697cd2f34962",
+    "url": "/my-resume/static/media/iress.0e92c8f0.jpg"
+  },
+  {
+    "revision": "9ea0d81547e28e85108f0e926bc130ba",
+    "url": "/my-resume/static/media/luxury-escapes-logo.9ea0d815.jpg"
+  },
+  {
+    "revision": "ea48ff3234d642379d88e50899e69b58",
+    "url": "/my-resume/static/media/optusSport.ea48ff32.png"
+  },
+  {
+    "revision": "e54bebe9dd0334c4d4b9cab67ec99379",
+    "url": "/my-resume/static/media/tab.e54bebe9.jpg"
+  }
+]);
